8eff91dfc4aea15b8c4b87d8cb4e5fc4e90570e3
Aidan <aw0345@gmail.com>
2018-01-06 16:34:39 +0000

    First commit

---
+++ app/src/main/java/com/example/user/gps_app/LocationData.java
@@ -0,0 +1,17 @@
+package com.example.user.gps_app;
+
+/**
+ * Created by User on 09/10/2017.
+ */
+
+public class LocationData {
+    double latitude;
+    double longitude;
+
+    public LocationData(double lat, double longt) {
+        this.longitude = longt;
+        this.latitude = lat;
+
+    }
+
+}
